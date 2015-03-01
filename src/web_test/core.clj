6706fec10f05535bd058c4ca6baddb85565e1224
zhujinxian <ainixian2004@qq.com>
2015-03-02 00:29:35 +0800

    test road 0.2.0

--- src/web_test/core.clj
+++ src/web_test/core.clj
@@ -1,8 +1,14 @@
 (ns web-test.core (:gen-class)
   (:use [road.router])
-  (:require [road.core :as road] 
+  (:require [road.core :as road]
+            [ring.middleware.params :as params]
+            [ring.util.response :as resp]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]))
+
+(defn render-test [ret tmt]
+  (-> (resp/response "------render----test------") 
+    (#(resp/content-type %1 "text/plain"))))
 
 (defn foo
   "I don't do a whole lot."
@@ -10,16 +16,18 @@
   (str "来自源码目录的参数：" x))
 
 (defn handler [^Integer x]
-    {:text (str "hello world, road goes sucess!" (foo x))})
+    {:$r render-test :text (str "hello world, road goes sucess!" (foo x))})
 
 (defn home [req content ^Integer num]
     {:hiccup "home.clj" :content (str "home" content) :num num})
 
-(defmacro mhandler [] 
-  (road/make-road-handler  aaa
-                          (GET "/web-test-0.1.0-SNAPSHOT-standalone/main" handler)
+
+(defn mhandler [] 
+  (road/defroad           (GET "/web-test-0.1.0-SNAPSHOT-standalone/main" handler)
                           (GET "/web-test-0.1.0-SNAPSHOT-standalone/home/:num{\\d+}" home)))
+
+(def road-handler (mhandler))
 
 (defn -main [& args]
   (println (mhandler))
-  (jetty/run-jetty (mhandler) {:port 3000}))
+  (jetty/run-jetty road-handler {:port 3000}))
